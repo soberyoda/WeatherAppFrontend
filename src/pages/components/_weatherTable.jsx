feb7a452674fd4032431db966a3c638463828c32
Agata Marczyk <agamarczyk.agh@gmail.com>
2024-05-06 19:11:02 +0200

    styling 3

--- src/pages/components/_weatherTable.jsx
+++ src/pages/components/_weatherTable.jsx
@@ -1,21 +1,37 @@
 import React, { useState } from 'react';
-import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Paper, useMediaQuery, Accordion, AccordionSummary, AccordionDetails, Typography, List, ListItem, ListItemText } from '@mui/material';
+import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Paper, useMediaQuery, Typography, List, ListItem, ListItemText, Grid } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import getWeatherIcon from './_weatherIcons';
+
+const WeatherTile = ({ day }) => {
+  return (
+    <Paper elevation={3} style={{ padding: '15px', marginBottom: '20px' }}>
+      <Typography variant="h6" style={{ marginBottom: '10px', color: '#7d7574' }}>Details for {day.date}</Typography>
+      <List>
+        <ListItem>
+          <ListItemText primary={`Max Temperature: ${day.maxTemperature}`} 
+            primaryTypographyProps={{ style: { color: '#7d7574' } }}/>
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Min Temperature: ${day.minTemperature}`} 
+            primaryTypographyProps={{ style: { color: '#7d7574' } }}/>
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Weather Icon:`} 
+            primaryTypographyProps={{ style: { color: '#7d7574' } }}/>{getWeatherIcon(day.weatherCode)}
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Estimated Energy (kW): ${day.estimatedEnergy}`} 
+            primaryTypographyProps={{ style: { color: '#7d7574' } }}/>
+        </ListItem>
+      </List>
+    </Paper>
+  );
+};
 
 const WeatherForecastTable = ({ weatherData }) => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-  const [expandedDay, setExpandedDay] = useState(null);
-
-  const handleDayExpand = (day) => {
-    if (expandedDay === day) {
-      setExpandedDay(null);
-    } else {
-      setExpandedDay(day);
-    }
-  };
 
   if (!weatherData) {
     return <p>Loading...</p>;
@@ -26,62 +42,36 @@ const WeatherForecastTable = ({ weatherData }) => {
       <Table>
         <TableHead>
           <TableRow style={{background: 'linear-gradient(135deg, #667eea 0%, #764ba2 100%)'}}>
-            <TableCell sx={{ fontSize: isMobile ? '14px' : 'inherit' }} align='center' style={{color:'white'}}>Date</TableCell>
+            <TableCell align='center' style={{color:'white'}}>Date</TableCell>
             {!isMobile && (
               <>
-                <TableCell sx={{ fontSize: isMobile ? '14px' : 'inherit' }} align='center' style={{color:'white'}}>Weather Icon</TableCell>
-                <TableCell sx={{ fontSize: isMobile ? '14px' : 'inherit' }} align='center' style={{color:'white'}}>Max Temperature</TableCell>
-                <TableCell sx={{ fontSize: isMobile ? '14px' : 'inherit' }} align='center' style={{color:'white'}}>Min Temperature</TableCell>
-                <TableCell sx={{ fontSize: isMobile ? '14px' : 'inherit' }} align='center' style={{color:'white'}}>Estimated Energy (kWh)</TableCell>
+                <TableCell align='center' style={{color:'white'}}>Weather Icon</TableCell>
+                <TableCell align='center' style={{color:'white'}}>Max Temperature</TableCell>
+                <TableCell align='center' style={{color:'white'}}>Min Temperature</TableCell>
+                <TableCell align='center' style={{color:'white'}}>Estimated Energy (kWh)</TableCell>
               </>
             )}
           </TableRow>
         </TableHead>
         <TableBody>
-          {weatherData.map(day => (
+          {weatherData.map((day, index) => (
             <React.Fragment key={day.date}>
-              <TableRow onClick={() => handleDayExpand(day.date)}>
-                <TableCell sx={{ fontSize: isMobile ? '14px' : 'inherit' }} align='center' style={{color:'#7d7574'}}>{day.date}</TableCell>
+              {isMobile && index === 0 && (
+                <Grid item xs={12}>
+                  <WeatherTile day={day} />
+                </Grid>
+              )}
+              <TableRow>
+                <TableCell align='center' style={{color:'#7d7574'}}>{day.date}</TableCell>
                 {!isMobile && (
                   <>
-                    <TableCell sx={{ fontSize: isMobile ? '14px' : 'inherit' }} align='center' style={{color:'#7d7574'}}>{getWeatherIcon(day.weatherCode)}</TableCell>
-                    <TableCell sx={{ fontSize: isMobile ? '14px' : 'inherit' }} align='center' style={{color:'#7d7574'}}>{day.maxTemperature}</TableCell>
-                    <TableCell sx={{ fontSize: isMobile ? '14px' : 'inherit' }} align='center' style={{color:'#7d7574'}}>{day.minTemperature}</TableCell>
-                    <TableCell sx={{ fontSize: isMobile ? '14px' : 'inherit' }} align='center' style={{color:'#7d7574'}}>{day.estimatedEnergy}</TableCell>
+                    <TableCell align='center' style={{color:'#7d7574'}}>{getWeatherIcon(day.weatherCode)}</TableCell>
+                    <TableCell align='center' style={{color:'#7d7574'}}>{day.maxTemperature}</TableCell>
+                    <TableCell align='center' style={{color:'#7d7574'}}>{day.minTemperature}</TableCell>
+                    <TableCell align='center' style={{color:'#7d7574'}}>{day.estimatedEnergy}</TableCell>
                   </>
                 )}
               </TableRow>
-              {isMobile && expandedDay === day.date && (
-                <TableRow>
-                  <TableCell colSpan={5}>
-                    <Accordion>
-                      <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
-                        <Typography style={{color:'#7d7574'}}>Details</Typography>
-                      </AccordionSummary>
-                      <AccordionDetails>
-                        <List>
-                          <ListItem>
-                            <ListItemText primary={`Max Temperature: ${day.maxTemperature}`} 
-                             primaryTypographyProps={{ style: { color: '#7d7574' } }}/>
-                          </ListItem>
-                          <ListItem>
-                            <ListItemText primary={`Min Temperature: ${day.minTemperature}`} 
-                            primaryTypographyProps={{ style: { color: '#7d7574' } }}/>
-                          </ListItem>
-                          <ListItem>
-                            <ListItemText primary={`Weather Icon:`} 
-                            primaryTypographyProps={{ style: { color: '#7d7574' } }}/>{getWeatherIcon(day.weatherCode)}
-                          </ListItem>
-                          <ListItem>
-                            <ListItemText primary={`Estimated Energy (kW): ${day.estimatedEnergy}`} 
-                            primaryTypographyProps={{ style: { color: '#7d7574' } }}/>
-                          </ListItem>
-                        </List>
-                      </AccordionDetails>
-                    </Accordion>
-                  </TableCell>
-                </TableRow>
-              )}
             </React.Fragment>
           ))}
         </TableBody>
